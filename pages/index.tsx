70317db722cdf65fb597a08ae4fd9a666f0acebd
afjk <afjk01@gmail.com>
2023-06-08 00:09:39 +0900

    api test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import Link from 'next/link'
+import { useEffect } from 'react';
 
 export default function IndexPage() {
+  useEffect(() => {
+    fetch('/api/user')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, []);
+
   return (
     <div>
       Hello World. by afjk <Link href="/about">About</Link>
